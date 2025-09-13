256438a7fbc1fcd6fd950b73b46fb24700a9ef7a
emanqarooni <emanqaroonibahrain@gmail.com>
2025-09-13 11:04:45 +0300

    added elements to html, style the html, and added the logic for timer and scoring and appear/disappear texts

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# project1
+# Bop the Cat
+## Date: 01/09/2025
+### By: Eman Qarooni
+#### [LinkedIn](www.linkedin.com/in/eman-qarooni) | [GitHub](https://github.com/emanqarooni)
+***
+### ***Description***
+##### A fun silly game that is inspired by whack-a-mole game but with the theme of cats and mouse, where the player should click on the cat to increase the score but when clicking on the mouse it will decrease the score.
+***
+### ***Technologies Used***
+* HTML
+* CSS
+* JavaScript
+* Visual Studio Code
+
+***
+### ***Getting Started***
+##### First I would create an html page that have all the essentials tags that is needed for my game: a title, score, timer, reset button, nine divs for the cells that the player clicks on.
+##### Second I would write basic css so that i can see my game works.
+##### Third I would initialize the global variables, create functions and event listener
+***
+### ***Screenshots***
+###### image header 1
+![Image](https://miro.medium.com/1*scJ2ib4AtOcNU2pH_7NmSQ.jpeg)
+
+***
+### ***Task List***
+- [ ] Initialize global variables
+- [ ] A grid of nine holes that will displays a cat/mouse appear and disappear.
+- [ ] If the user clicks the mouse, they lose a point.
+- [ ] If the player clicks on the cat, increase a point.
+- [ ] display score and timer.
+- [ ] Game ends when time runs out.
+- [x] ~~~Completed~~~ Update
+***
+### ***Credit***
+##### Reference 1: [www.miro.com](https://miro.medium.com/1*scJ2ib4AtOcNU2pH_7NmSQ.jpeg)
+
---
+++ script.js
@@ -0,0 +1,87 @@
+// global variables
+let time = 20 //initializing the timer
+let score = 0 //initializing the score
+let target = 5 //initializing how many target the player should hit for winning the game
+let strikes = 0 //initializing the number of strikes when the player hits the mouse
+
+// connecting html elements
+const timeElement = document.querySelector("#time")
+const scoreElement = document.querySelector("#score")
+const targetElement = document.querySelector("#target")
+const resultElement = document.querySelector("#result")
+const strikesElement = document.querySelector("#strikes")
+const squares = document.querySelectorAll(".smallSquare")
+
+//showing the values of each variable right away after starting the game
+timeElement.innerText = time
+scoreElement.innerText = score
+targetElement.innerText = target
+strikesElement.innerText = strikes
+
+// these variables will store the interval ids so we can stop them later
+let gameTimer //used for counting down the time and clearing the time
+let animalTimer //used for showing a new animal every second and stop showing the animals
+
+// a function that start the game
+const startGame = () => {
+  //I saw this way of counting down the timer from a reddit comment from this post: https://www.reddit.com/r/learnjavascript/comments/ql9yes/how_to_set_up_a_timer_in_javascript/
+  //and I understood more about set intervals and how to clear them from w3schools https://www.w3schools.com/jsref/met_win_setinterval.asp and https://www.w3schools.com/js/js_timing.asp
+  //so in this function the timer goes down each second (1000) and for each second it will be shown on the website and if the timer reached zero or less then it is game over.
+
+  gameTimer = setInterval(() => {
+    time -= 1 //the counter goes down by a second
+    timeElement.innerText = time //each second that counts down will be overwritten
+
+    //if the timer reaches zero it will call the end game function and the game will be over
+    if (time <= 0) {
+      endGame(false)
+    }
+  }, 1000)
+
+  // Show animal any animal every second
+  animalTimer = setInterval(showAnimal, 1000)
+}
+
+// show a random animal
+const showAnimal = () => {
+  // clear all squares first
+  squares.forEach((sq) => {
+    sq.textContent = ""
+    sq.onclick = null
+  })
+
+  // player can pick any random square
+  let randomIndex = Math.floor(Math.random() * squares.length)
+  let randomSquare = squares[randomIndex]
+
+  // check the probability of the animal showing randomly on each square
+  let randomNumber = Math.random()
+  let animal
+  if (randomNumber < 0.7) {
+    animal = "cat" // 70% chance
+  } else {
+    animal = "mouse" // 30% chance
+  }
+
+  // show the animal text
+  randomSquare.innerText = animal
+
+  //I used the onclick event instead of addEventListener because addEventListener keeps adding new listeners every time the square is updated. That means if I don’t remove them properly, a single click could trigger multiple functions and increase the score or strikes more than once. Using onclick is simpler here because it automatically overwrites the old click handler, so each square only responds once at a time.
+  //https://www.geeksforgeeks.org/javascript/difference-between-addeventlistener-and-onclick-in-javascript/
+  randomSquare.onclick = () => {
+    if (animal === "cat") {
+      score++
+    } else {
+      strikes++
+      strikesElement.innerText = strikes
+    }
+    scoreElement.innerText = score
+
+    // immediately clear the square after click which prevents double scoring
+    randomSquare.innerText = ""
+    randomSquare.onclick = null
+  }
+}
+
+// Start immediately
+startGame()
